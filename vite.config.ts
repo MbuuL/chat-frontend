416fce1bc0ae34ae3c42bfc9c4a863099f709785
Alif Robby Alcafi <arifu.arukafi@gmail.com>
2026-02-06 07:57:53 +0000

    Add tailwind import

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
@@ -10,5 +11,6 @@ export default defineConfig({
       autoCodeSplitting: true,
     }),
     react(),
+    tailwindcss(),
   ],
 });
